e38290cc473746a1afdd442c04586dea1ac15c84
johanPaC115 <johanandrescp@ufps.edu.co>
2021-01-23 18:29:38 -0500

    POJO Mascota para mapulacion del DataSet

---
+++ app/src/main/java/com/johancap115/materialdesignintro/Mascota.java
@@ -0,0 +1,39 @@
+package com.johancap115.materialdesignintro;
+
+import android.content.Intent;
+
+public class Mascota {
+    private int foto;
+    private String nombre;
+    private int raiting;
+
+    public Mascota(int foto, String nombre) {
+        this.foto = foto;
+        this.nombre = nombre;
+        this.raiting = 0;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRaiting() {
+        return raiting;
+    }
+
+    public void setRaiting(int raiting) {
+        this.raiting = raiting;
+    }
+}
