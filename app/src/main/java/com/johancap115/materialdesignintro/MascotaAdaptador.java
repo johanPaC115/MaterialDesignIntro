4c83b1f6a6b3c36bc6c049b44eb3325c702d90fb
johanPaC115 <johanandrescp@ufps.edu.co>
2021-01-25 18:39:21 -0500

    Adaptador para Cada Item del RecyclerView

---
+++ app/src/main/java/com/johancap115/materialdesignintro/MascotaAdaptador.java
@@ -0,0 +1,67 @@
+package com.johancap115.materialdesignintro;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder>{
+
+    ArrayList<Mascota> mascotas;
+    ItemListener itemListener;
+
+    public MascotaAdaptador(ArrayList<Mascota> mascotas, ItemListener itemlistener) {
+        this.mascotas = mascotas;
+        this.itemListener = itemlistener;
+    }
+
+    @NonNull
+    @Override
+    public MascotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_mascota, parent, false);
+        return new MascotaViewHolder(v);
+    }
+
+    //Asocia cada elemento de la lista con cada View
+    @Override
+    public void onBindViewHolder(@NonNull MascotaViewHolder mascotaViewHolder, int position) {
+        Mascota mascota = mascotas.get(position);
+        mascotaViewHolder.imgFotoCV.setImageResource(mascota.getFoto());
+        mascotaViewHolder.txvNombreCV.setText(mascota.getNombre());
+        mascotaViewHolder.txvRaitingCV.setText(String.valueOf(mascota.getRaiting()));
+
+        mascotaViewHolder.imgRaitingCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemListener.onClick(mascota);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() { //Cantidad de elementos que contiene mi Lista
+        return mascotas.size();
+    }
+
+    public static class MascotaViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView imgFotoCV;
+        private TextView txvNombreCV;
+        private  TextView txvRaitingCV;
+        private ImageView imgRaitingCV;
+
+        public MascotaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgFotoCV    = (ImageView) itemView.findViewById(R.id.imgPet);
+            txvNombreCV  = (TextView) itemView.findViewById(R.id.tvNpmbrePet);
+            txvRaitingCV = (TextView) itemView.findViewById(R.id.tvRaitingPet);
+            imgRaitingCV = (ImageView) itemView.findViewById(R.id.imgLikeHueso);
+        }
+    }
+}
