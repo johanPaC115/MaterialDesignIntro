57d3aebdb5455dc1fb8a048e7aaf91066463fea0
johanPaC115 <johanandrescp@ufps.edu.co>
2021-02-05 12:14:17 -0500

    Organizando pakages

--- app/src/main/java/com/johancap115/materialdesignintro/ItemListener.java
+++
@@ -1,5 +0,0 @@
-package com.johancap115.materialdesignintro;
-
-public interface ItemListener {
-    void onClick(Mascota pet);
-}
---
+++ app/src/main/java/com/johancap115/materialdesignintro/interfaz/ItemListener.java
@@ -0,0 +1,7 @@
+package com.johancap115.materialdesignintro.interfaz;
+
+import com.johancap115.materialdesignintro.pojo.Mascota;
+
+public interface ItemListener {
+    void onClick(Mascota pet);
+}
