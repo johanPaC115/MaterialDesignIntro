ccb1b651c7c772506d3fa9b85bc266af28467bab
johanPaC115 <johanandrescp@ufps.edu.co>
2021-02-10 15:48:25 -0500

    Add Config Autenticacion Mail

---
+++ app/src/main/java/com/johancap115/materialdesignintro/Util/ConfigMail.java
@@ -0,0 +1,9 @@
+package com.johancap115.materialdesignintro.Util;
+
+public class ConfigMail {
+    //This is your from email
+    public static final String EMAIL = "your-gmail";
+
+    //This is your from email password
+    public static final String PASSWORD = "your-gmail-password";
+}
